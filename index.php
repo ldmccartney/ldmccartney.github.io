24857e299bfcd99607ee30bce728acfd13fd3fba
Lennon McCartney <mccartney.photo@gmail.com>
2015-12-03 16:44:58 -0500

    scripted push

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+ 
+  <ul>
+    <li><a href="index.html">Home</a></li>
+  </ul>
+
+<!-- <?php include 'navbar.php';?> -->
+
+
+  <body>
+    <h1>Hello World</h1>
+    <p>I'm hosted with Github Pages</p>
+  </body>
+</html>
